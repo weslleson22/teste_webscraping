4a5dddf510cb4909f117f393b11f3abd4ff305c6
Wesleson22 <wesllesonsowza@gmail.com>
2022-06-15 15:35:48 -0300

    teste

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://example.com');
   await page.screenshot({ path: 'example.png' });
-//as
+//W
   await browser.close();
 })();
