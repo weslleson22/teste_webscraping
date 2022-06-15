05074c826d2a734e95227a81758058b9e2dec321
Wesleson22 <wesllesonsowza@gmail.com>
2022-06-15 15:56:56 -0300

    codigo

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+require('dotenv').config();
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
-//W
-  await browser.close();
-})();
+  await page.goto('https://unsplash.com/');
+
+  // - Acessa a página de login
+  await page.click('[href="/login"]');
+
+  // Troque os valores de process.env.UNSPLASH_EMAIL e process.env.UNSPLASH_PASS pelo seu login e senha :)
+  await page.type('[name="user[email]"]', process.env.UNSPLASH_EMAIL)
+  await page.type('#user_password', process.env.UNSPLASH_PASS)
+
+  await page.click('[type="submit"]')
+
+  await page.waitForNavigation();
+
+  // ACESSAR essa pagina
+  await page.goto('https://unsplash.com/photos/LzWXPcJg7lk');
+
+  // Like nessa coisa
+  await page.click('[title="Like photo"]')
